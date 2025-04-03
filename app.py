c80acb001f287cea946d3b2f704d4eba96a3d83b
Smit Patel <75073602+Smit1963@users.noreply.github.com>
2025-04-03 19:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def convert_pdf_to_image(uploaded_file):
     return pdf_parts
 
 # Setup Streamlit app
-st.set_page_config(page_title="RezUp - Resume Optimizer", layout="wide", page_icon="🤖")
+st.set_page_config(page_title="RezUp - Resume Optimizer", layout="wide", page_icon="📋")
 
 # Custom CSS styling with larger fonts
 st.markdown("""
